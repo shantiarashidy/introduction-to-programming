4e119d0eae6ec336f57fc3e666134e6e74ea0c97
Shantia <shantiarashidy12@gmail.com>
2025-12-02 20:48:02 +0330

    Add files via upload

---
+++ PR-S1-NO1-Area-of-a-circle.cpp
@@ -0,0 +1,13 @@
+// Write a program to calculate the Area of ​​a circle with radius of 10
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    float Area;
+    float pi = 3.14;
+    int radius = 10;
+
+    Area = pi * radius * radius;
+    cout <<"The Area of circle is : " << Area;
+}
